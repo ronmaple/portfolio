9741b3f343fef217a47c657f5148af479a36f92b
Ron Mapue <ronmapue@gmail.com>
2018-07-14 14:52:30 -0700

    5th commit - temporarily||permanently disable jquery as it possibly interrupts deployment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import 'jquery';
+// import jquery from 'jquery';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.js';
+// import 'bootstrap/dist/js/bootstrap.js';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
